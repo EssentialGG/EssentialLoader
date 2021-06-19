ec4b274054a81286e0e55f2a7a98dab5924a8758
Jonas Herzig <jonas@spark-squared.com>
2021-06-19 10:21:50 +0200

    Tweaker: Automatically detect core mods

--- src/main/java/gg/essential/loader/EssentialSetupTweaker.java
+++ src/main/java/gg/essential/loader/EssentialSetupTweaker.java
@@ -7,21 +7,24 @@ import net.minecraftforge.common.ForgeVersion;
 import net.minecraftforge.fml.relauncher.CoreModManager;
 
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.security.CodeSource;
 import java.util.List;
+import java.util.jar.JarFile;
 
 @SuppressWarnings("unused")
 public class EssentialSetupTweaker implements ITweaker {
     private final EssentialLoader loader;
 
     public EssentialSetupTweaker() {
-        this(new String[0]);
+        this(null);
     }
 
+    @Deprecated
     public EssentialSetupTweaker(String[] fmlPlugins) {
         String version = "unknown";
 
@@ -47,6 +50,9 @@ public class EssentialSetupTweaker implements ITweaker {
 
             Method loadCoreMod = CoreModManager.class.getDeclaredMethod("loadCoreMod", LaunchClassLoader.class, String.class, File.class);
             loadCoreMod.setAccessible(true);
+            if (fmlPlugins == null) {
+                fmlPlugins = getCoreMods(currentFile);
+            }
             for (String s : fmlPlugins) {
                 ((List<ITweaker>) Launch.blackboard.get("Tweaks")).add((ITweaker) loadCoreMod.invoke(null, Launch.classLoader, s, currentFile));
 
@@ -54,6 +60,18 @@ public class EssentialSetupTweaker implements ITweaker {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private String[] getCoreMods(File file) throws IOException {
+        try (JarFile jar = new JarFile(file)) {
+            if (jar.getManifest() != null) {
+                String fmlCorePlugin = jar.getManifest().getMainAttributes().getValue("FMLCorePlugin");
+                if (fmlCorePlugin != null) {
+                    return new String[] { fmlCorePlugin };
+                }
+            }
+        }
+        return new String[0];
     }
 
     private File getCurrentFile() throws URISyntaxException {
