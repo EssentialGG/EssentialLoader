0d90a1b58d5853003c639389f5b54c7f1127bb25
Jonas Herzig <jonas@spark-squared.com>
2021-08-14 14:14:10 +0200

    Test: Remove TestableLaunch
    Instead we build the regular args array, add the exclusions via reflection and
    add the re-throw instead of exit via ASM.

--- integrationTest/common/src/main/java/gg/essential/loader/fixtures/IsolatedLaunch.java
+++ integrationTest/common/src/main/java/gg/essential/loader/fixtures/IsolatedLaunch.java
@@ -7,11 +7,14 @@ import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.InsnNode;
 import org.objectweb.asm.tree.LdcInsnNode;
+import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
 
-import java.io.File;
 import java.io.InputStream;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import java.net.JarURLConnection;
 import java.net.URL;
 import java.net.URLClassLoader;
@@ -25,14 +28,36 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class IsolatedLaunch {
+    private static final String LAUNCH_CLASS_NAME = "net.minecraft.launchwrapper.Launch";
     private final IsolatedClassLoader loader = new IsolatedClassLoader();
 
     public void launch(Path gameDir, String tweaker) throws Exception {
         System.out.println("Launching " + tweaker + " in " + gameDir);
+
         System.setSecurityManager(new NoExitAllowed());
-        Class<?> cls = getClass("gg.essential.loader.fixtures.TestableLaunch");
-        Object obj = cls.newInstance();
-        cls.getDeclaredMethod("launch", File.class, String.class).invoke(obj, gameDir.toFile(), tweaker);
+        try {
+            Class<?> cls = getClass(LAUNCH_CLASS_NAME);
+
+            Constructor<?> constructor = cls.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            Object obj = constructor.newInstance();
+
+            Object launchClassLoader = cls.getDeclaredField("classLoader").get(null);
+            Method addExclusion = launchClassLoader.getClass().getDeclaredMethod("addClassLoaderExclusion", String.class);
+            addExclusion.invoke(launchClassLoader, "com.example.mod.LoadState");
+            addExclusion.invoke(launchClassLoader, "com.example.mod2.LoadState");
+            addExclusion.invoke(launchClassLoader, "gg.essential.LoadState");
+            addExclusion.invoke(launchClassLoader, "com.google.common.jimfs.");
+
+            Method launchMethod = cls.getDeclaredMethod("launch", String[].class);
+            launchMethod.setAccessible(true);
+            launchMethod.invoke(obj, (Object) new String[] {
+                "--gameDir", gameDir.toString(),
+                "--tweakClass", tweaker,
+            });
+        } finally {
+            System.setSecurityManager(new EverythingIsAllowedSecurityManager());
+        }
     }
 
     public boolean getModLoadState(String field) throws Exception {
@@ -95,7 +120,9 @@ public class IsolatedLaunch {
                     // Fuck forge
                     // We need to remove the SecurityManager lock down.. and it refuses to boot if we reject its manager
                     // (re-defining the whole package so we can ignore the signature)
-                    if (name.startsWith("net.minecraftforge.fml.")) {
+                    // Unrelated to forge, we also want to remove the System.exit(1) from Launch and just re-throw any
+                    // exceptions so we get the error directly in the test result rather than having to look at the log.
+                    if (name.startsWith("net.minecraftforge.fml.") || name.equals(LAUNCH_CLASS_NAME)) {
                         URL jarUrl;
                         byte[] bytes;
                         try {
@@ -107,17 +134,26 @@ public class IsolatedLaunch {
                         } catch (Exception e) {
                             throw new ClassNotFoundException(name, e);
                         }
-                        if (name.equals("net.minecraftforge.fml.relauncher.FMLSecurityManager")) {
+                        boolean isFMLTweaker = name.equals("net.minecraftforge.fml.common.launcher.FMLTweaker");
+                        boolean isLaunch = name.equals(LAUNCH_CLASS_NAME);
+                        if (isFMLTweaker || isLaunch) {
                             ClassNode classNode = new ClassNode(Opcodes.ASM5);
                             new ClassReader(bytes).accept(classNode, 0);
                             for (MethodNode method : classNode.methods) {
                                 InsnList instructions = method.instructions;
                                 for (int i = 0; i < instructions.size(); i++) {
                                     AbstractInsnNode insn = instructions.get(i);
-                                    if (insn instanceof LdcInsnNode) {
-                                        LdcInsnNode ldcInsn = (LdcInsnNode) insn;
-                                        if ("setSecurityManager".equals(ldcInsn.cst)) {
-                                            ldcInsn.cst = "ignored";
+                                    if (isFMLTweaker) {
+                                        // This removes any call to setSecurityManager, instead dropping the security manager
+                                        if (insn instanceof MethodInsnNode && ((MethodInsnNode) insn).name.equals("setSecurityManager")) {
+                                            instructions.set(insn, new InsnNode(Opcodes.POP));
+                                        }
+                                    }
+                                    if (isLaunch) {
+                                        // This inserts a (re-)throw before the error is handed to the logger
+                                        if (insn instanceof LdcInsnNode && "Unable to launch".equals(((LdcInsnNode) insn).cst)) {
+                                            instructions.insertBefore(insn, new InsnNode(Opcodes.ATHROW));
+                                            i++;
                                         }
                                     }
                                 }
@@ -134,6 +170,16 @@ public class IsolatedLaunch {
                 classes.put(name, cls);
                 return cls;
             }
+        }
+    }
+
+    private static class EverythingIsAllowedSecurityManager extends SecurityManager {
+        @Override
+        public void checkPermission(Permission perm) {
+        }
+
+        @Override
+        public void checkPermission(Permission perm, Object context) {
         }
     }
 
