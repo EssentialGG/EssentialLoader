d289facce32f8574adb325cd6bd1252b17690dd2
Jonas Herzig <jonas@spark-squared.com>
2021-10-17 17:25:31 +0200

    stage1/lw: Add three more dev env tests
    A minimal one without any additional third-party mod (like the existing one).
    And a variant of each of those two with relaunching (these were fixed by the
    previous two commits).

--- integrationTest/launchwrapper/src/main/java/gg/essential/loader/Stage1Tests.java
+++ integrationTest/launchwrapper/src/main/java/gg/essential/loader/Stage1Tests.java
@@ -288,16 +288,57 @@ public class Stage1Tests {
         assertTrue(isolatedLaunch.isEssentialLoaded(), "Essential loaded");
     }
 
-    @Test
-    public void testEssentialTweakerModsInDev(Installation installation) throws Exception {
-        installation.addExample2Mod("essential-tweaker");
-
+    private IsolatedLaunch newDevLaunch(Installation installation) throws Exception {
         IsolatedLaunch isolatedLaunch = installation.newLaunchFML();
         isolatedLaunch.addToClasspath(installation.stage0JarFile.toUri().toURL());
         isolatedLaunch.addToClasspath(installation.mixin07JarFile.toUri().toURL());
         isolatedLaunch.addToClasspath(Paths.get("build", "classes", "java", "exampleMod").toUri().toURL());
         isolatedLaunch.addArg("--tweakClass", "gg.essential.loader.stage0.EssentialSetupTweaker");
         isolatedLaunch.setProperty("fml.coreMods.load", "com.example.mod.ExampleCoreMod");
+        return isolatedLaunch;
+    }
+
+    @Test
+    public void testInDev(Installation installation) throws Exception {
+        IsolatedLaunch isolatedLaunch = newDevLaunch(installation);
+        isolatedLaunch.launch();
+
+        assertTrue(isolatedLaunch.getModLoadState("coreMod"), "Example CoreMod ran");
+        assertTrue(isolatedLaunch.getModLoadState("mod"), "Example Mod ran");
+        assertTrue(isolatedLaunch.isEssentialLoaded(), "Essential loaded");
+    }
+
+    @Test
+    public void testEssentialTweakerModsInDev(Installation installation) throws Exception {
+        installation.addExample2Mod("essential-tweaker");
+
+        IsolatedLaunch isolatedLaunch = newDevLaunch(installation);
+        isolatedLaunch.launch();
+
+        assertTrue(isolatedLaunch.getModLoadState("coreMod"), "Example CoreMod ran");
+        assertTrue(isolatedLaunch.getModLoadState("mod"), "Example Mod ran");
+        assertTrue(isolatedLaunch.getMod2LoadState("coreMod"), "Example2 CoreMod ran");
+        assertTrue(isolatedLaunch.getMod2LoadState("mod"), "Example2 Mod ran");
+        assertTrue(isolatedLaunch.isEssentialLoaded(), "Essential loaded");
+    }
+
+    @Test
+    public void testInDevWithRelaunch(Installation installation) throws Exception {
+        IsolatedLaunch isolatedLaunch = newDevLaunch(installation);
+        isolatedLaunch.setProperty("essential.branch", "asm-52");
+        isolatedLaunch.launch();
+
+        assertTrue(isolatedLaunch.getModLoadState("coreMod"), "Example CoreMod ran");
+        assertTrue(isolatedLaunch.getModLoadState("mod"), "Example Mod ran");
+        assertTrue(isolatedLaunch.isEssentialLoaded(), "Essential loaded");
+    }
+
+    @Test
+    public void testEssentialTweakerModsInDevWithRelaunch(Installation installation) throws Exception {
+        installation.addExample2Mod("essential-tweaker");
+
+        IsolatedLaunch isolatedLaunch = newDevLaunch(installation);
+        isolatedLaunch.setProperty("essential.branch", "asm-52");
         isolatedLaunch.launch();
 
         assertTrue(isolatedLaunch.getModLoadState("coreMod"), "Example CoreMod ran");
