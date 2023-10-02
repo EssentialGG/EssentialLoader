7644f6e0e22a83a069c9e53a117b6e11e0931792
Jonas Herzig <jonas@spark-squared.com>
2023-10-02 18:13:28 +0200

    build: Use GitHub Actions to run tests
    GitHub: #14

--- stage2/common/src/main/java/gg/essential/loader/stage2/jvm/ForkedJvmLoaderSwingUI.java
+++ stage2/common/src/main/java/gg/essential/loader/stage2/jvm/ForkedJvmLoaderSwingUI.java
@@ -2,9 +2,19 @@ package gg.essential.loader.stage2.jvm;
 
 import gg.essential.loader.stage2.LoaderSwingUI;
 
+import java.awt.*;
 import java.io.IOException;
 
 public class ForkedJvmLoaderSwingUI extends ForkedJvmLoaderUI {
+    @Override
+    public void start() {
+        // Skip actual GUI for integration tests if we're in a headless environment
+        if (System.getProperty("essential.integration_testing") != null && GraphicsEnvironment.isHeadless()) {
+            return;
+        }
+        super.start();
+    }
+
     public static void main(String[] args) throws IOException {
         forked(new LoaderSwingUI());
     }
