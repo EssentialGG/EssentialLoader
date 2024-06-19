54c2da3364adec0267493ae661136ee9dd840f34
sychic <47618543+Sychic@users.noreply.github.com>
2024-06-19 12:05:10 -0700

    stage1/ml9: switch back to looking for `ITransformationService`

--- stage1/modlauncher9/src/main/java/gg/essential/loader/stage1/EssentialMixinPluginLoader.java
+++ stage1/modlauncher9/src/main/java/gg/essential/loader/stage1/EssentialMixinPluginLoader.java
@@ -2,6 +2,7 @@ package gg.essential.loader.stage1;
 
 import cpw.mods.modlauncher.Launcher;
 import cpw.mods.modlauncher.api.IEnvironment;
+import cpw.mods.modlauncher.api.ITransformationService;
 import cpw.mods.modlauncher.api.TypesafeMap;
 import net.minecraftforge.fml.loading.FMLLoader;
 
@@ -13,15 +14,13 @@ public class EssentialMixinPluginLoader {
     private static final String KEY_LOADED = "gg.essential.loader.stage1.loaded";
 
     public EssentialMixinPluginLoader() throws Exception {
-        // Check if another stage1 loader has already loaded stage2 (we do not want to load it twice)
+        // Check if another transformation service has already loaded stage2 (we do not want to load it twice)
         final TypesafeMap blackboard = Launcher.INSTANCE.blackboard();
-        final TypesafeMap.Key<Object> LOADED =
-                TypesafeMap.Key.getOrCreate(blackboard, KEY_LOADED, Object.class);
+        final TypesafeMap.Key<ITransformationService> LOADED =
+                TypesafeMap.Key.getOrCreate(blackboard, KEY_LOADED, ITransformationService.class);
         if (blackboard.get(LOADED).isPresent()) {
             return;
         }
-        // We are doing it
-        blackboard.computeIfAbsent(LOADED, __ -> this);
 
         final Path gameDir = Launcher.INSTANCE.environment()
                 .getProperty(IEnvironment.Keys.GAMEDIR.get())
