3554d8f48da911ca5c16cb004ce9a45314b854e4
Jonas Herzig <jonas@spark-squared.com>
2025-05-07 11:43:38 +0200

    stage2/lw: Handle embedded stage1 via transformer instead of modifying the jar

--- stage2/launchwrapper/src/main/java/gg/essential/loader/stage2/relaunch/RelaunchTransformer.java
+++ stage2/launchwrapper/src/main/java/gg/essential/loader/stage2/relaunch/RelaunchTransformer.java
@@ -9,6 +9,8 @@ import org.objectweb.asm.tree.InsnList;
 import org.objectweb.asm.tree.InsnNode;
 import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.TypeInsnNode;
+import org.objectweb.asm.tree.VarInsnNode;
 
 import java.util.function.BiFunction;
 
@@ -30,6 +32,33 @@ public class RelaunchTransformer implements BiFunction<String, byte[], byte[]> {
                     if (insn instanceof MethodInsnNode && ((MethodInsnNode) insn).name.equals("setSecurityManager")) {
                         instructions.set(insn, new InsnNode(Opcodes.POP));
                     }
+                }
+            }
+            ClassWriter classWriter = new ClassWriter(Opcodes.ASM5);
+            classNode.accept(classWriter);
+            return classWriter.toByteArray();
+        }
+
+        // Suppress the stage1 update mechanism in any stage0 classes because it'll fail on Windows where the file
+        // is already locked by the JVM.
+        if (name.endsWith(".EssentialSetupTweaker")) {
+            ClassNode classNode = new ClassNode(Opcodes.ASM5);
+            new ClassReader(bytes).accept(classNode, 0);
+            for (MethodNode method : classNode.methods) {
+                if ("loadStage1".equals(method.name)) {
+                    // Older stage0 versions have a static method, in newer ones it's a member method.
+                    boolean isStatic = (method.access & Opcodes.ACC_STATIC) != 0;
+                    InsnList instructions = method.instructions;
+                    instructions.clear();
+                    instructions.add(new TypeInsnNode(Opcodes.NEW, "gg/essential/loader/stage1/EssentialSetupTweaker"));
+                    instructions.add(new InsnNode(Opcodes.DUP));
+                    instructions.add(new VarInsnNode(Opcodes.ALOAD, isStatic ? 0 : 1));
+                    instructions.add(new MethodInsnNode(Opcodes.INVOKESPECIAL, "gg/essential/loader/stage1/EssentialSetupTweaker", "<init>", "(Lnet/minecraft/launchwrapper/ITweaker;)V", false));
+                    instructions.add(new InsnNode(Opcodes.ARETURN));
+                    method.maxLocals = isStatic ? 1 : 2;
+                    method.maxStack = 3;
+                    method.localVariables.clear();
+                    method.tryCatchBlocks.clear();
                 }
             }
             ClassWriter classWriter = new ClassWriter(Opcodes.ASM5);
