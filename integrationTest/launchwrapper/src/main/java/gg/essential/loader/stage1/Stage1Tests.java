3fcef5456039e82caf2e019cd366f4fe6e5d9021
Jonas Herzig <jonas@spark-squared.com>
2025-07-07 15:50:42 +0200

    tests/lw: Move tweaker-specific tests into dedicated file

--- integrationTest/launchwrapper/src/main/java/gg/essential/loader/stage1/Stage1Tests.java
+++ integrationTest/launchwrapper/src/main/java/gg/essential/loader/stage1/Stage1Tests.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -53,32 +52,6 @@ public class Stage1Tests {
 
         installation.assertModLaunched(isolatedLaunch);
         assertEquals(secondLaunch, isolatedLaunch.isEssentialLoaded(), "Essential loaded");
-    }
-
-    @Test
-    public void testMultipleCustomTweakerMods(Installation installation) throws Exception {
-        installation.addExampleMod();
-        installation.addExample2Mod();
-
-        IsolatedLaunch isolatedLaunch = installation.launchFML();
-
-        installation.assertModLaunched(isolatedLaunch);
-        installation.assertMod2Launched(isolatedLaunch);
-        assertTrue(isolatedLaunch.isEssentialLoaded(), "Essential loaded");
-    }
-
-    @Test
-    public void testMultipleEssentialTweakerMods(Installation installation) throws Exception {
-        installation.addExampleMod("essential-tweaker");
-        installation.addExample2Mod("essential-tweaker");
-
-        IsolatedLaunch isolatedLaunch = installation.launchFML();
-
-        assertTrue(isolatedLaunch.getModLoadState("coreMod"), "Example CoreMod ran");
-        assertTrue(isolatedLaunch.getModLoadState("mod"), "Example Mod ran");
-        assertTrue(isolatedLaunch.getMod2LoadState("coreMod"), "Example2 CoreMod ran");
-        assertTrue(isolatedLaunch.getMod2LoadState("mod"), "Example2 Mod ran");
-        assertTrue(isolatedLaunch.isEssentialLoaded(), "Essential loaded");
     }
 
     @Test
