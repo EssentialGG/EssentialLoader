81d8d58fd07b5bafdb55c33f0392e913a02ea739
Jonas Herzig <jonas@spark-squared.com>
2025-07-07 14:08:36 +0200

    tests/lw: Drop requirement for Essential to be excluded from LaunchClassLoader
    This has been fixed in Essential in
    ea9aa48c5e9058e4525f23200b68e90411b0dda4 (dated 2021).

--- integrationTest/launchwrapper/src/essential/java/gg/essential/api/tweaker/EssentialTweaker.java
+++ integrationTest/launchwrapper/src/essential/java/gg/essential/api/tweaker/EssentialTweaker.java
@@ -1,7 +1,6 @@
 package gg.essential.api.tweaker;
 
 import sun.gg.essential.LoadState;
-import net.minecraft.launchwrapper.Launch;
 
 import java.io.File;
 
@@ -9,12 +8,5 @@ import java.io.File;
 public class EssentialTweaker {
     public static void initialize(File gameDir) {
         LoadState.tweaker = true;
-
-        ClassLoader expectedLoader = Launch.classLoader.getClass().getClassLoader();
-        ClassLoader actualLoader = EssentialTweaker.class.getClassLoader();
-        if (expectedLoader != actualLoader) {
-            throw new RuntimeException("Essential should be excluded from the Launch class loader." +
-                "Expected: " + expectedLoader + ", but was " + actualLoader);
-        }
     }
 }
