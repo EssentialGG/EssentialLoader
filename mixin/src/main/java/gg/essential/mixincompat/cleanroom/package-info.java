0faedcbfb9d31aa00aef5d6a72ccd15b172081f7
Jonas Herzig <jonas@spark-squared.com>
2025-07-14 13:20:38 +0200

    mixin: Add support for MixinBooter
    This commit adds stubs and custom functionality as required by
    MixinBooter to not crash when booting.
    
    Mods which rely on other features only available in MixinBooter's Mixin
    fork or which rely on the Mixin version shipped with MixinBooter, may of
    course still not function properly. Although we are not currently aware
    of any such mod.

---
+++ mixin/src/main/java/gg/essential/mixincompat/cleanroom/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package contains patches to make Cleanroom's MixinBooter (which relies Cleanroom's Mixin fork) not crash
+ * (whether it is fully functional is a different question; it at least appears to be mostly functional)
+ * when its custom Mixin fork is replaced by our patched Mixin.
+ *
+ * @link https://github.com/CleanroomMC/MixinBooter/tree/05fc6c7b4b36a714c90eb4cc2f2364c681da0bc8/
+ * @link https://github.com/CleanroomMC/MixinBooter-UniMix/tree/9d4b487ed32501137645cdf0da484b076f0bfaf4/
+ */
+package gg.essential.mixincompat.cleanroom;
