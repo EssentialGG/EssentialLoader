a56b26f219d6e05ea857120921f82f66bef5f108
Traben <ben.trabant@gmail.com>
2026-02-23 19:35:19 +1000

    stage2/fabric: Support custom FabricLoader impl subclasses in injectFakeMod
    This is the case with e.g. Lunar Client's custom Fabric Loader implementation.
    
    Linear: EM-3472
    GitHub: #28

--- stage2/fabric/src/main/java/gg/essential/loader/stage2/EssentialLoader.java
+++ stage2/fabric/src/main/java/gg/essential/loader/stage2/EssentialLoader.java
@@ -347,10 +347,30 @@ public class EssentialLoader extends EssentialLoaderBase {
             }
         }
 
+        private Class<?> findFabricLoaderClass(FabricLoader fabricLoader) throws ClassNotFoundException {
+            Class<?> clazz = fabricLoader.getClass();
+            Exception ex = null;
+            while (clazz != null) {
+                try {
+                    clazz.getDeclaredField("modMap");
+                    clazz.getDeclaredField("mods");
+                    clazz.getDeclaredField("entrypointStorage");
+                    clazz.getDeclaredField("adapterMap");
+                    return clazz;
+                } catch (NoSuchFieldException e) {
+                    if (ex == null) ex = e;
+                    else ex.addSuppressed(e); // Accumulate exceptions to provide more context
+
+                    clazz = clazz.getSuperclass();
+                }
+            }
+            throw new ClassNotFoundException("Could not find the required fields [modMap, mods, entrypointStorage, adapterMap] anywhere in the class hierarchy of FabricLoader.getInstance()", ex);
+        }
+
         @SuppressWarnings("unchecked")
         private void injectFakeMod(final Path path, final URL url, final ModMetadata metadata) throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException, ClassNotFoundException, InstantiationException {
             FabricLoader fabricLoader = FabricLoader.getInstance();
-            Class<? extends FabricLoader> fabricLoaderClass = fabricLoader.getClass();
+            Class<?> fabricLoaderClass = findFabricLoaderClass(fabricLoader);
             Class<?> ModContainerImpl;
             try {
                 // fabric-loader 0.12
